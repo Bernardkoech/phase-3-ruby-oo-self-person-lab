28f7c8f890626a4329de4a3114c5964d0508f626
Bernard <bernard.koech@student.moringaschool.com>
2023-02-23 17:17:25 +0300

    complete

--- lib/p.rb
+++ lib/p.rb
@@ -1,7 +1,6 @@
-# your code goes here
 class Person
-  attr_reader :name
-  attr_accessor :bank_account, :happiness, :hygiene
+  attr_reader :name, :happiness, :hygiene
+  attr_accessor :bank_account
 
   def initialize(name)
     @name = name
@@ -26,15 +25,20 @@ class Person
     hygiene > 7
   end
 
+  def get_paid(salary)
+    self.bank_account += salary
+    "all about the benjamins"
+  end
+
   def take_bath
     self.hygiene += 4
     "♪ Rub-a-dub just relaxing in the tub ♫"
   end
 
   def work_out
-    self.hygiene -= 3
     self.happiness += 2
-    '♪ another one bites the dust ♫'
+    self.hygiene -= 3
+    "♪ another one bites the dust ♫"
   end
 
   def call_friend(friend)
@@ -43,36 +47,26 @@ class Person
     "Hi #{friend.name}! It's #{self.name}. How are you?"
   end
 
-  def get_paid(amount)
-    self.bank_account += amount
-    "all about the benjamins"
-  end
-
   def start_conversation(friend, topic)
     case topic
     when "politics"
       [self, friend].each { |person| person.happiness -= 2 }
-     "blah blah partisan blah lobbyist"
+      "blah blah partisan blah lobbyist"
     when "weather"
-      [self, friend].each {|person| person.happiness += 1}
-      'blah blah sun blah rain'
+      [self, friend].each { |person| person.happiness += 1 }
+      "blah blah sun blah rain"
     else
-      'blah blah blah blah blah'
-
+      "blah blah blah blah blah"
     end
   end
 
   def self.limit_value(value)
-    case
-    when value > 10
+    if value > 10
       10
-
-    when value < 0
+    elsif value < 0
       0
-
     else
       value
     end
   end
-
 end
